98292636f78153bfc5d4423599dbf59077f3b026
renebec <39104197+renebec@users.noreply.github.com>
2024-08-24 13:10:54 -0600

    Add files via upload

---
+++ docs/actions.js
@@ -0,0 +1,108 @@
+function PopWinClose()
+{
+    window.close();
+}
+
+function PopWinHome()
+{
+    window.open("index.html","width=100%", "_self");
+}
+
+
+function PopWinA1()
+{
+    window.open("popA1.html","width=100%", "_self");
+}
+
+function PopWinA2()
+{
+    window.open("popA2.html","width=100%", "_self");
+}
+
+
+function PopWinA1s1()
+{
+    window.open("popA1s1.html","width=100%", "_self");
+}
+
+
+function PopWinA1s2()
+{
+    window.open("popA1s2.html","width=100%", "_self");
+}
+
+function PopWinA1s3()
+{
+    window.open("popA1s3.html","width=100%", "_self");
+}
+
+function PopWinA1r1()
+{
+    floatingWindow = window.open("popA1r1.html","","height=400,width=400,scrollbars=yes");
+}
+
+
+function PopWinA1s1_2()
+{
+    window.open("popA1s1_2.html","width=100%", "_self");
+}
+
+
+function PopWinA1r2()
+{
+    floatingWindow = window.open("popA1r2.html","","height=400,width=400,scrollbars=yes");
+}
+
+
+function PopWinA1s1_2w1()
+{
+    window.open("popA1s1_2w1.html","","height=400,width=400,scrollbars=yes");
+}
+
+
+function PopWinA1s1_2w2()
+{
+    window.open("popA1s1_2w2.html","","height=400,width=400,scrollbars=yes");
+}
+
+
+function PopWinA1s1_3()
+{
+    window.open("popA1s1_3.html","width=100%", "_self");
+}
+
+function PopWinA1r3()
+{
+    floatingWindow = window.open("popA1r3.html","","height=400,width=400,scrollbars=yes");
+}
+
+function PopWinA1r4()
+{
+    floatingWindow = window.open("popA1r4.html","","height=400,width=400,scrollbars=yes");
+}
+
+function PopWinA1s1_4()
+{
+    window.open("popA1s1_4.html","width=100%", "_self");
+}
+
+
+
+function PopWinA1s1_5()
+{
+    window.open("popA1s1_5.html","width=100%", "_self");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
